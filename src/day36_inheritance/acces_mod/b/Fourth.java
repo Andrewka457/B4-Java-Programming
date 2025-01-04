8e0d6eb69fbb8ee39c31a6430316798b6acf6671
Andrewka <spryginandrei@gmail.com>
2025-01-04 14:53:08 -0500

    new practice

---
+++ src/day36_inheritance/acces_mod/b/Fourth.java
@@ -0,0 +1,23 @@
+package day36_inheritance.acces_mod.b;
+
+import day36_inheritance.acces_mod.a.First;
+
+public class Fourth {
+
+    public static void main(String[] args) {
+
+        First f = new First();
+        System.out.println(f.one);
+//        System.out.println(f.two);
+//        System.out.println(f.three);
+//        System.out.println(f.four);
+
+        Fourth f4 = new Fourth();
+//        System.out.println(f4.one);
+//        System.out.println(f4.two);
+//        System.out.println(f4.three);
+//        System.out.println(f4.four);
+
+    }
+
+}
---
+++ src/day36_inheritance/taxi/Lyft.java
@@ -0,0 +1,16 @@
+package day36_inheritance.taxi;
+
+public class Lyft {
+
+    String driverName;
+
+    public Lyft(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public double calculateRate(int miles) {
+        return miles * 2.5;
+    }
+
+
+}
